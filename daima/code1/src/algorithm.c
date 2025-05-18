9fc12ffa2968a793c73dcfb2a17405febd744855
pianfei <1206033215@qq.com>
2025-05-18 09:38:16 +0800

    1

--- daima/code1/src/algorithm.c
+++ daima/code1/src/algorithm.c
@@ -94,7 +94,7 @@ void Cardiology1(int r, int c)
     Pair oS,oE;
    // u8 aBuf[21];
 
-    //为什么程序崩溃，价格link.h头文件就好了？
+    //为什么程序崩溃，加个link.h头文件就好了？
     stablePos = (AnsStru*)RawMalloc(sizeof(AnsStru) * c);
 
 //    for(i=0;i<r*c;i++)
